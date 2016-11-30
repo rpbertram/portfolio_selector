7aafc5539dad74ae5ff29f9a62625f26d22251ba
Raghu Betina <raghu@firstdraft.com>
2016-11-30 05:03:32 +0000

    Direct association between funds and historicalperformances

--- app/models/historicalperformance.rb
+++ app/models/historicalperformance.rb
@@ -1,6 +1,10 @@
 class Historicalperformance < ApplicationRecord
   # Direct associations
 
+  belongs_to :moneymanager,
+             :class_name => "Fund",
+             :foreign_key => "fund_id"
+
   # Indirect associations
 
   # Validations
