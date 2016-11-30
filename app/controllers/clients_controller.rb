059cf7c0b1277a08535ae8f7153eb347d9f241d1
Raghu Betina <raghu@firstdraft.com>
2016-11-30 05:04:00 +0000

    Only client user can modify client

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,14 @@
 class ClientsController < ApplicationController
+  before_action :current_user_must_be_client_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_client_advisor
+    client = Client.find(params[:id])
+
+    unless current_user == client.advisor
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @clients = Client.all
 
