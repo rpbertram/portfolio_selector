d09b4b1824d4dd70e331f5c7d7e7c2e479149106
Raghu Betina <raghu@firstdraft.com>
2016-11-30 05:03:32 +0000

    Direct association between funds and allocations

--- app/models/allocation.rb
+++ app/models/allocation.rb
@@ -1,6 +1,10 @@
 class Allocation < ApplicationRecord
   # Direct associations
 
+  belongs_to :moneymanager,
+             :class_name => "Fund",
+             :foreign_key => "fund_id"
+
   belongs_to :client
 
   # Indirect associations
